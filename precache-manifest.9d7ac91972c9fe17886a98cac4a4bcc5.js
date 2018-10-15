b0d928f29fc1601b079745e7a1c3334b05029d4d
Sandesh Poudel <curious.sandesh@gmail.com>
2018-10-15 23:21:03 +0300

    Updates

---
+++ precache-manifest.9d7ac91972c9fe17886a98cac4a4bcc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f33ec54d873227d21578",
+    "url": "/git_repo/static/js/runtime~main.f33ec54d.js"
+  },
+  {
+    "revision": "97bae5910837b858ac37",
+    "url": "/git_repo/static/js/main.97bae591.chunk.js"
+  },
+  {
+    "revision": "ed30c52411a0dd912513",
+    "url": "/git_repo/static/js/1.ed30c524.chunk.js"
+  },
+  {
+    "revision": "ed30c52411a0dd912513",
+    "url": "/git_repo/static/css/1.58f9b0a1.chunk.css"
+  },
+  {
+    "revision": "b106351c7121b30348187b3fb6284292",
+    "url": "/git_repo/index.html"
+  }
+];
